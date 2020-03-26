e9fc35f23cd174597cfa6457a6dc66a84f5818e7
Pablo Andrés Díaz <pabloandresd@gmail.com>
2020-03-26 09:57:06 -0500

    Se ha agregado el proyecto del backend

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## Taller de React 2020
 
 Para ejecutar el Frontend:
-- npm run dev
+npm run dev
 
+Para ejecutar el Backend:
+dotnet run --project .\API\API.csproj
---
+++ backend/API/Controllers/PacienteController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PacienteController : ControllerBase
+    {
+        private readonly ILogger<PacienteController> _logger;
+
+        public PacienteController(ILogger<PacienteController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("perfil")]
+        public string ObtenerPerfil()
+        {
+            return "Este es el perfil";
+        }
+    }
+}
